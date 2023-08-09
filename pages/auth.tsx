744fd0a33de066ff7f840e775e50b2ada426fd26
NoctriLi <96703319+NoctriLi@users.noreply.github.com>
2023-08-09 18:12:52 -0400

    disable auth

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,124 +1,124 @@
-import { useCallback, useState } from "react";
-import axios from "axios";
-import Input from "@/app/_components/Input";
-import { signIn } from "next-auth/react";
+// import { useCallback, useState } from "react";
+// import axios from "axios";
+// import Input from "@/app/_components/Input";
+// import { signIn } from "next-auth/react";
 
-import { FcGoogle } from "react-icons/fc";
-import { FaGithub } from "react-icons/fa";
+// import { FcGoogle } from "react-icons/fc";
+// import { FaGithub } from "react-icons/fa";
 
-const Auth = () => {
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
+// const Auth = () => {
+//   const [email, setEmail] = useState("");
+//   const [name, setName] = useState("");
+//   const [password, setPassword] = useState("");
 
-  const [variant, setVariant] = useState("login");
-  const toggleVariant = useCallback(() => {
-    setVariant((prev) => (prev === "login" ? "register" : "login"));
-  }, []);
+//   const [variant, setVariant] = useState("login");
+//   const toggleVariant = useCallback(() => {
+//     setVariant((prev) => (prev === "login" ? "register" : "login"));
+//   }, []);
 
-  const login = useCallback(async () => {
-    try {
-      await signIn("credentials", {
-        email,
-        password,
-        callbackUrl: "/profiles",
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  }, [email, password]);
+//   const login = useCallback(async () => {
+//     try {
+//       await signIn("credentials", {
+//         email,
+//         password,
+//         callbackUrl: "/profiles",
+//       });
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   }, [email, password]);
 
-  const register = useCallback(async () => {
-    try {
-      await axios.post("/api/register", {
-        email,
-        name,
-        password,
-      });
+//   const register = useCallback(async () => {
+//     try {
+//       await axios.post("/api/register", {
+//         email,
+//         name,
+//         password,
+//       });
 
-      login();
-    } catch (error) {
-      console.log(error);
-    }
-  }, [email, name, password, login]);
+//       login();
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   }, [email, name, password, login]);
 
-  return (
-    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
-      <div className="bg-black h-full w-full lg:bg-opacity-50">
-        <nav className="px-12 py-5">
-          <img src="/images/logo.png" alt="Webflix Logo" className="h-12" />
-        </nav>
-        <div className="flex justify-center">
-          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
-            <h2 className="text-white text-4xl mb-8 font-semibold">
-              {variant === "login" ? "Login" : "Register"}
-            </h2>
-            <div className="flex flex-col gap-4">
-              {variant === "register" && (
-                <Input
-                  label="Username"
-                  onChange={(ev: any) => setName(ev.target.value)}
-                  id="name"
-                  value={name}
-                />
-              )}
-              <Input
-                label="Email"
-                onChange={(ev: any) => setEmail(ev.target.value)}
-                id="email"
-                type="email"
-                value={email}
-              />
-              <Input
-                label="Password"
-                onChange={(ev: any) => setPassword(ev.target.value)}
-                id="password"
-                value={password}
-              />
-            </div>
-            <button
-              onClick={variant === "login" ? login : register} ////////////////////////////////////////login/register/////////////
-              className="bg-red-600 text-white rounded-md w-full py-3 mt-10 hover:bg-red-700 transition"
-            >
-              {variant === "login" ? "Login" : "Sign up"}
-            </button>
-            <div className="flex items-center justify-center gap-4 mt-6">
-              <div className="h-px w-16 bg-gray-700"></div>
-              <p className="text-gray-500">or</p>
-              <div className="h-px w-16 bg-gray-700"></div>
-            </div>
+//   return (
+//     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+//       <div className="bg-black h-full w-full lg:bg-opacity-50">
+//         <nav className="px-12 py-5">
+//           <img src="/images/logo.png" alt="Webflix Logo" className="h-12" />
+//         </nav>
+//         <div className="flex justify-center">
+//           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+//             <h2 className="text-white text-4xl mb-8 font-semibold">
+//               {variant === "login" ? "Login" : "Register"}
+//             </h2>
+//             <div className="flex flex-col gap-4">
+//               {variant === "register" && (
+//                 <Input
+//                   label="Username"
+//                   onChange={(ev: any) => setName(ev.target.value)}
+//                   id="name"
+//                   value={name}
+//                 />
+//               )}
+//               <Input
+//                 label="Email"
+//                 onChange={(ev: any) => setEmail(ev.target.value)}
+//                 id="email"
+//                 type="email"
+//                 value={email}
+//               />
+//               <Input
+//                 label="Password"
+//                 onChange={(ev: any) => setPassword(ev.target.value)}
+//                 id="password"
+//                 value={password}
+//               />
+//             </div>
+//             <button
+//               onClick={variant === "login" ? login : register} ////////////////////////////////////////login/register/////////////
+//               className="bg-red-600 text-white rounded-md w-full py-3 mt-10 hover:bg-red-700 transition"
+//             >
+//               {variant === "login" ? "Login" : "Sign up"}
+//             </button>
+//             <div className="flex items-center justify-center gap-4 mt-6">
+//               <div className="h-px w-16 bg-gray-700"></div>
+//               <p className="text-gray-500">or</p>
+//               <div className="h-px w-16 bg-gray-700"></div>
+//             </div>
 
-            <div className="flex flex-row items-center justify-center gap-4 mt-6">
-              <div
-                onClick={() => signIn("google", { callbackUrl: "/profiles" })}
-                className="w-10 h-10 flex items-center justify-center bg-white rounded-full py-3  hover:opacity-80 transition"
-              >
-                <FcGoogle size={30} />
-              </div>
-              <div
-                onClick={() => signIn("github", { callbackUrl: "/profiles" })}
-                className="w-10 h-10 flex items-center justify-center bg-white rounded-full py-3  hover:opacity-80 transition"
-              >
-                <FaGithub size={30} />
-              </div>
-            </div>
+//             <div className="flex flex-row items-center justify-center gap-4 mt-6">
+//               <div
+//                 onClick={() => signIn("google", { callbackUrl: "/profiles" })}
+//                 className="w-10 h-10 flex items-center justify-center bg-white rounded-full py-3  hover:opacity-80 transition"
+//               >
+//                 <FcGoogle size={30} />
+//               </div>
+//               <div
+//                 onClick={() => signIn("github", { callbackUrl: "/profiles" })}
+//                 className="w-10 h-10 flex items-center justify-center bg-white rounded-full py-3  hover:opacity-80 transition"
+//               >
+//                 <FaGithub size={30} />
+//               </div>
+//             </div>
 
-            <p className="text-neutral-500 mt-12">
-              {variant === "login"
-                ? "Don't have an account?"
-                : "Already have an account?"}
-              <span
-                onClick={toggleVariant}
-                className="text-white ml-1 hover:underline cursor-pointer"
-              >
-                {variant === "login" ? "Sign up" : "Login"}
-              </span>
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+//             <p className="text-neutral-500 mt-12">
+//               {variant === "login"
+//                 ? "Don't have an account?"
+//                 : "Already have an account?"}
+//               <span
+//                 onClick={toggleVariant}
+//                 className="text-white ml-1 hover:underline cursor-pointer"
+//               >
+//                 {variant === "login" ? "Sign up" : "Login"}
+//               </span>
+//             </p>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Auth;
+// export default Auth;
