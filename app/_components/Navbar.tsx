7b4c10037fab09431a0c583846dfd4db4d905326
NoctriLi <96703319+NoctriLi@users.noreply.github.com>
2023-08-20 00:27:01 -0400

    mobile navbar fixed

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -7,16 +7,16 @@ const Navbar = () => {
     <nav className="fixed w-full z-[100] h-20 flex flex-col cols-3 opacity-[80%] bg-zinc-900 ">
       <div className="flex flex-row navpanel justify-center mx-2 basis-7/12 ">
 
-        <div className=" text-center align-middle p-2 border-x border-white ">
+        <div className="flex text-center align-middle p-2 border-x border-white ">
             <h1 className="text-white">SIGN IN | REGISTER</h1>
         </div>
 
-        <div className="navlogo justify-center">
+        <div className="justify-center">
           <h1 className="text-white text-3xl text-center">LOGO</h1>
         </div>
 
         <div className="flex flex-row gap-2 p-2 border border-white">
-          <h1 className="text-white ">search</h1>
+          
           <SearchInput />
         </div>
 
--- app/_components/SearchInput.tsx
+++ app/_components/SearchInput.tsx
@@ -3,23 +3,41 @@
 import React from "react";
 
 const SearchInput = () => {
-    const [searchTerm, setSearchTerm] = React.useState("");
+  const [searchTerm, setSearchTerm] = React.useState("");
 
-    const onSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setSearchTerm(e.target.value);
-    };
+  const onSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
 
-
-
-    return (
-        <div className="flex flex-col relative gap-0 min-w-[100px] h-fit text-white text-xs rounded shadow ">
-            <div className="relative">
-                <input type="text" placeholder="Search" value={searchTerm} onChange={onSearch}/>
-
-            </div>
-        </div>
-    );
-
-    };
+  return (
+    <div className="flex flex-col relative gap-0 min-w-fit h-fit text-white text-xs rounded shadow ">
+      <div className="hidden lg:block relative">
+      <h1 className="text-white ">search</h1>
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={onSearch}
+        />
+      </div>
+      <div className="block lg:hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+          />
+        </svg>
+      </div>
+    </div>
+  );
+};
 
 export default SearchInput;
