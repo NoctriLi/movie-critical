35038e7b2e884a9ee991e5db2d77ea2a8ff7e416
NoctriLi <96703319+NoctriLi@users.noreply.github.com>
2023-10-12 15:30:01 -0400

    fixed nav spacing

--- app/_components/nav/Navbar.tsx
+++ app/_components/nav/Navbar.tsx
@@ -3,38 +3,34 @@ import SearchInput from './SearchInput'
 import { ModeToggle } from '@/components/ui/DarkModeToggle'
 
 const Navbar = () => {
-    return (
-        <nav className="cols-3 fixed z-[300] flex h-20 w-full flex-col bg-secondary text-foreground opacity-[80%] ">
-            <div className="navpanel mx-2 flex basis-7/12 flex-row justify-center ">
-                <div className='flex items'>
+  return (
+    <nav className="cols-3 fixed z-[300] flex  h-20 w-full bg-secondary text-foreground opacity-[80%] ">
+      <div className="mx-2 flex flex-row  justify-between items-center w-full ">
+        <div className="items flex">
+          <div className="h-fit w-fit px-5">
+            <ModeToggle />
+          </div>
+        </div>
 
+        <a
+          href="/"
+          className=" hidden overflow-hidden text-center text-3xl  md:block"
+        >
+          Movie Critical
+        </a>
+        <a
+          href="/"
+          className=" block overflow-hidden text-center text-3xl md:hidden"
+        >
+          M.Crit
+        </a>
 
-                    <div className='px-5 w-fit h-fit'>
-                        <ModeToggle />
-                    </div>
-                </div>
-
-                
-                    <a
-                        href="/"
-                        className=" hidden overflow-hidden text-center text-3xl  md:block"
-                    >
-                        Movie Critical
-                    </a>
-                    <a
-                        href="/"
-                        className=" block overflow-hidden text-center text-3xl md:hidden"
-                    >
-                        M.Crit
-                    </a>
-                
-
-                <div className="flex p-2">
-                    <SearchInput />
-                </div>
-            </div>
-        </nav>
-    )
+        <div className="flex p-2">
+          <SearchInput />
+        </div>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
